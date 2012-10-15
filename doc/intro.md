11dc10d5eece81037893369f86582b0710d34731
ds300 <ds300@sussex.ac.uk>
2012-10-15 13:37:42 +0100

    new project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to nlp-trie
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/nlp_trie/core_test.clj
@@ -0,0 +1,7 @@
+(ns nlp-trie.core-test
+  (:use clojure.test
+        nlp-trie.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
